0458ba7bcd0260db104409e4e78a3cb9c98e47da
Robert Kroeger <rjkroege@chromium.org>
2017-10-18 21:10:55 -0400

    Modified mainline to generate
    Modified the mainline code to actually generate mount scripts.

--- cmd.go
+++ cmd.go
@@ -3,19 +3,15 @@ package main
 import (
 	"log"
 
-	"github.com/rjkroege/mountgenerator"
+	"github.com/rjkroege/mountgenerator/mntgen"
 )
 
 func main() {
-	log.Println("hello")
-
-	disks, err := mountgenerator.EnumerateDisks("/dev/disk/by-id")	
+	disks, err := mntgen.EnumerateDisks("/dev/disk/by-id")	
 	if err != nil {
 		// TODO(rjk): make sure that mountgenerator fails end up in the system
 		// log. Maybe they will by default.
 		log.Fatalln("mountgenerator had a sad", err)
 	}
-
-
-
+	mntgen.ForAllDisks(disks, mntgen.ServicedUnitsLocation)
 }
